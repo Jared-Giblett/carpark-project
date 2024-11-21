9f6a07541724fee1d0e89b6e9e3deecd9543b6b9
20135031 <20135031@tafe.wa.edu.au>
2024-11-21 16:16:41 +0800

    feat: init method for car_park

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,10 @@
-class CarPark
-    pass
+class CarPark:
+    def __init__(self, location = "Unknown", capacity = 100, plates = None, sensors = None, displays = None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates
+        self.sensors = sensors
+        self.displays = displays
+
+    def __str___(self):
+        return f"Car park at {self.location}, with {self.capacity} bays."
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,2 @@
-class Sensor
+class Sensor:
     pass
