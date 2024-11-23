8b96ff6f9a46f486d86c0bb6308bc5a4c2d0c606
20135031 <20135031@tafe.wa.edu.au>
2024-11-23 13:28:16 +0800

    feat: Fleshed out sensor

--- src/sensor.py
+++ src/sensor.py
@@ -1,14 +1,42 @@
-class Sensor:
-    def __init__(self, id=404, is_active=False, car_park=None):
+from abc import ABC, abstractmethod
+import random
+
+class Sensor(ABC):
+    def __init__(self, id, is_active=False, car_park=None):
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
     def __str__(self):
-        return f"Senser {self.id} is {self.is_active}"
+        return f"Sensor {self.id} is {self.is_active}"
+
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
+    def _scan_plate(self)
+        return 'FAKE-' + format(random.randint(0, 999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
 
 class EntrySenser(Sensor):
-    ...
+    def detect_vehicle(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming vehicle detected: Plate :{plate}")
+
+    def update_car_park(self, plate):
+        ...
 
 class ExitSenser(Sensor):
-    ...
+    def detect_vehicle(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing vehicle detected: Plate :{plate}")
+
+
+    def update_car_park(self, plate):
+        ...
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
